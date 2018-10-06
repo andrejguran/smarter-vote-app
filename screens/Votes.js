8ed67481e3520dc58313d4b5d7b1da0675d9e8df
Andrej Guran <andrejguran@gmail.com>
2018-10-06 16:33:14 +0200

    votes

--- screens/Votes.js
+++ screens/Votes.js
@@ -1,5 +1,19 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button, ImageBackground} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button, ImageBackground, ScrollView, TouchableHighlight} from 'react-native';
+
+
+const styles = {
+  text: {
+    color: 'white',
+    marginTop: 165,
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 5,
+    textShadowColor: 'rgba(0, 0, 0, 1)',
+  textShadowOffset: {width: -2, height: 2},
+  textShadowRadius: 10
+  }
+}
 
 export default class Votes extends React.Component {
     static navigationOptions = {
@@ -8,19 +22,54 @@ export default class Votes extends React.Component {
     render() {
       const { navigate } = this.props.navigation;
       return (
-        <View style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-        }}>
-          <ImageBackground onPress={() =>
-              navigate('Vote', { id: 1 })
-            } source={{
-    uri: 'https://facebook.github.io/react/logo-og.png'
-  }}style={{width: 150, height: 270}}>
-          <Text>laalaal</Text>
+        <ScrollView>
+   
+   <View style={{flex: 1, flexDirection: 'row', justifyContent:'space-evenly', marginTop: 20}}>
+          
+          <TouchableHighlight onPress={() => navigate('Vote', { id: 1 })}>
+          <ImageBackground  source={require('../img/law1.jpg')}style={{width: 170, height: 250}}>
+          <Text  style={styles.text}>H.R.302: FAA Reauthorization Act of 2018</Text>
           </ImageBackground>
-        </View>
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => navigate('Vote', { id: 2 })}>
+          <ImageBackground  source={require('../img/law5.png')}style={{width: 170, height: 250}}>
+          <Text style={styles.text}>H.R.6157: Department of Defense</Text>
+          </ImageBackground>
+          </TouchableHighlight>
+
+</View>
+<View style={{flex: 1, flexDirection: 'row', justifyContent:'space-evenly', marginTop: 20}}>
+          
+          <TouchableHighlight onPress={() => navigate('Vote', { id: 1 })}>
+          <ImageBackground  source={require('../img/law6.png')}style={{width: 170, height: 250}}>
+          <Text style={styles.text}>H.R.6: SUPPORT for Patients and Communities Act</Text>
+          </ImageBackground>
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => navigate('Vote', { id: 1 })}>
+          <ImageBackground  source={require('../img/law4.png')}style={{width: 170, height: 250}}>
+          <Text style={styles.text}>H.R.6147 - Making appropriations for the Department</Text>
+          </ImageBackground>
+          </TouchableHighlight>
+
+</View>
+<View style={{flex: 1, flexDirection: 'row', justifyContent:'space-evenly', marginTop: 20}}>
+          
+          <TouchableHighlight onPress={() => navigate('Vote', { id: 1 })}>
+          <ImageBackground  source={require('../img/law2.png')}style={{width: 170, height: 250}}>
+          <Text style={styles.text}>H.R.303: FAA Reauthorization Act of 2018</Text>
+          </ImageBackground>
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => navigate('Vote', { id: 1 })}>
+          <ImageBackground  source={require('../img/law3.png')}style={{width: 170, height: 250}}>
+          <Text style={styles.text}>H.R.304: FAA Reauthorization Act of 2018</Text>
+          </ImageBackground>
+          </TouchableHighlight>
+
+</View>
+        </ScrollView>
       );
     }
   }
